7a2f9ffc44ea79e308cb9d4bae94dca062704783
Neal <sunwx0602@163.com>
2019-05-29 14:42:11 +0800

    First to learn RabbitMQ

---
+++ overweight/src/main/java/com/sun/overweight/OverweightApplication.java
@@ -0,0 +1,13 @@
+package com.sun.overweight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverweightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OverweightApplication.class, args);
+    }
+
+}
