4dadc2b8e679fb2f678f57db495043185841ca15
sunwx33102 <sunwx33102@hundsun.com>
2022-10-22 15:38:21 +0800

    123

--- overweight/src/test/java/com/sun/overweight/OverweightApplicationTests1.java
+++ overweight/src/test/java/com/sun/overweight/OverweightApplicationTests1.java
@@ -30,19 +30,31 @@ public class OverweightApplicationTests1 {
         List<RemoveHandler> input = new ArrayList<>();
         RemoveHandler removeHandler1 = RemoveHandler.builder().insNum("a").value(new BigDecimal("10.1")).build();
         RemoveHandler removeHandler2 = RemoveHandler.builder().insNum("b").value(new BigDecimal("30.3")).build();
+        RemoveHandler removeHandler6 = RemoveHandler.builder().insNum("g").value(null).build();
         RemoveHandler removeHandler3 = RemoveHandler.builder().insNum("c").value(new BigDecimal("-20")).build();
+        RemoveHandler removeHandler5 = RemoveHandler.builder().insNum("f").value(new BigDecimal("-20")).build();
+        RemoveHandler removeHandler9 = RemoveHandler.builder().insNum("j").value(new BigDecimal("-30")).build();
+        RemoveHandler removeHandler10 = RemoveHandler.builder().insNum("k").value(new BigDecimal("-20")).build();
         RemoveHandler removeHandler4 = RemoveHandler.builder().insNum("d").value(null).build();
+        RemoveHandler removeHandler7 = RemoveHandler.builder().insNum("h").value(new BigDecimal("11.2")).build();
+        RemoveHandler removeHandler8 = RemoveHandler.builder().insNum("i").value(new BigDecimal("11.2")).build();
         input.add(removeHandler1);
         input.add(removeHandler2);
+        input.add(removeHandler5);
+        input.add(removeHandler6);
+        input.add(removeHandler9);
+        input.add(removeHandler10);
+        input.add(removeHandler7);
+        input.add(removeHandler8);
         input.add(removeHandler3);
         input.add(removeHandler4);
-        Boolean flag = true;
-//        Boolean flag = false;
-        if (flag){
+//        Boolean flag = true;
+        Boolean flag = false;
+        if (flag) {
             // 从小到大-nulllast
             input.sort(Comparator.nullsLast(Comparator.comparing(RemoveHandler::getValue,
                     Comparator.nullsLast(BigDecimal::compareTo))));
-        }else {
+        } else {
             // 从大到小-nulllast
             input.sort(Comparator.nullsLast(Comparator.comparing(RemoveHandler::getValue,
                     Comparator.nullsFirst(BigDecimal::compareTo)).reversed()));
@@ -61,8 +73,37 @@ public class OverweightApplicationTests1 {
 //            list.add(currentMap);
 //        }
 //        List<RemoveHandler> pxlist = new ArrayList<>();
+        BigDecimal flagNum = null;
+        int rankNum = 1;
+        int sameNum = 0;
         for (int i = 0; i < sortInput.size(); i++) {
-            System.out.println("insNum:" + sortInput.get(i) + ";排名:" + i );
+            RemoveHandler removeHandler = sortInput.get(i);
+            BigDecimal aa = removeHandler.getValue();
+            if (i == 0) {
+                flagNum = aa;
+                System.out.println("insNum:" + removeHandler + ";排名one:" + rankNum);
+            } else {
+                if (aa != null && flagNum != null && flagNum.compareTo(aa) != 0) {
+                    flagNum = aa;
+                    rankNum = rankNum + 1;
+                    rankNum = rankNum + sameNum;
+                    System.out.println("insNum:" + removeHandler + ";排名two:" + rankNum);
+                    sameNum = 0;
+                } else if (aa == null && flagNum != null) {
+                    flagNum = aa;
+                    rankNum++;
+                    rankNum = rankNum + sameNum;
+                    System.out.println("insNum:" + removeHandler + ";排名three:" + rankNum);
+                    sameNum = 0;
+                } else if (aa != null && flagNum != null && flagNum.compareTo(aa) == 0) {
+                    flagNum = aa;
+                    sameNum = sameNum + 1;
+                    System.out.println("insNum:" + removeHandler + ";排名four:" + rankNum);
+                } else if (aa == null) {
+                    rankNum = rankNum + sameNum;
+                    System.out.println("insNum:" + removeHandler + ";排名five:" + rankNum);
+                }
+            }
 
         }
 
