e726266f59eaf3ce721c2e7f25e8184ac8bd96b7
sunwx33102 <sunwx33102@hundsun.com>
2023-10-31 15:27:45 +0800

    123

--- overweight/src/test/java/com/sun/overweight/OverweightApplicationTests2.java
+++ overweight/src/test/java/com/sun/overweight/OverweightApplicationTests2.java
@@ -11,9 +11,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.CollectionUtils;
 
+import java.io.*;
 import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
+
+import org.apache.commons.math3.linear.*;
+
 
 /**
  * @param
@@ -30,21 +34,40 @@ public class OverweightApplicationTests2 {
 
     @Test
     public static void main(String[] args) throws Exception {
-        int targetSum = 12;
-        int targetValue = 88;
 
-        for (int a = 0; a <= targetSum; a++) {
-            for (int b = 0; b <= targetSum; b++) {
-                for (int c = 0; c <= targetSum; c++) {
-                    for (int d = 0; d <= targetSum; d++) {
-                        if (a + b + c + d == targetSum && a + 5 * b + 10 * c + 20 * d == targetValue) {
-                            System.out.println("一元 = " + a + ", 五元 = " + b + ", 十元 = " + c + ", 二十元 = " + d);
-                        }
+            String targetFolderPath = "C:\\Users\\hspcadmin\\Desktop\\数据\\prc"; // 替换为实际的目标文件夹路径
+            String outputFilePath = "C:\\Users\\hspcadmin\\Desktop\\数据\\prc_tmp.sql"; // 输出文件路径
+
+            try {
+                mergeSQLFiles(targetFolderPath, outputFilePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+    }
+
+    public static void mergeSQLFiles(String folderPath, String outputFilePath) throws IOException {
+        File folder = new File(folderPath);
+        File[] files = folder.listFiles((dir, name) -> name.endsWith(".sql"));
+
+        if (files == null || files.length == 0) {
+            System.out.println("没有找到任何.sql文件");
+            return;
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+            for (File file : files) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        writer.write(line);
+                        writer.newLine();
                     }
                 }
             }
         }
 
-        System.out.println("No solution found.");
+        System.out.println("合并完成，输出文件：" + outputFilePath);
     }
 }
--- overweight/src/test/java/com/sun/overweight/OverweightApplicationTests5.java
+++ overweight/src/test/java/com/sun/overweight/OverweightApplicationTests5.java
@@ -5,6 +5,27 @@ import com.sun.overweight.common.utils.CommUtil;
 import com.sun.overweight.common.utils.DateUtil;
 import com.sun.overweight.controller.UserController;
 import org.junit.Test;
+import org.apache.poi.xwpf.usermodel.*;
+import org.apache.poi.util.Units;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTBody;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTDocument1;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTParagraph;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTP;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTbl;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblRow;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblCell;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTcPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTR;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTPicture;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTInline;
+import org.w3c.dom.*;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.*;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +34,32 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
+
+import org.apache.poi.xwpf.usermodel.*;
+import org.apache.poi.util.Units;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 
 /**
  * @param
@@ -23,71 +70,51 @@ import java.util.stream.Collectors;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OverweightApplicationTests5 {
-    @Autowired
-    public static UserController userController;
-
-
-    @Test
     public static void main(String[] args) throws Exception {
-        int size = 133;
-        for (int i = 0; i < size; i++) {
-            System.out.println(CommUtil.uuid());
-        }
-        String a = "";
-        String b = null;
-        String c = a+ b +"hej";
-        System.out.println(c);
+        String templatePath = "template_word_path"; // 模板文件路径
+        String outputPath = "output_word_path"; // 输出文件路径
+        String imagePath = "image_path"; // 图片文件路径
+        String[] data = {"张三", "男", "25"}; // 需要填充的数据
+
+        fillWordTemplate(templatePath, outputPath, imagePath, data);
     }
 
+    public static void fillWordTemplate(String templatePath, String outputPath, String imagePath, String[] data) throws Exception {
+        // 读取模板文件
+        FileInputStream fis = new FileInputStream(new File(templatePath));
+        XWPFDocument document = new XWPFDocument(fis);
+        fis.close();
 
-    /**
-     * 求在百分位p的值是多少
-     *
-     * @param dataList
-     * @param p
-     * @return
-     */
-    private static double getPercentile(List<Double> dataList, double p) {
-        int n = dataList.size();
-        dataList.sort(new Comparator<Double>() {
-            //从小到大排序
-            @Override
-            public int compare(Double o1, Double o2) {
-                if (o1 == null || o2 == null) {
-                    return 0;
+        // 获取正文内容
+        List<XWPFParagraph> paragraphs = document.getParagraphs();
+        for (XWPFParagraph paragraph : paragraphs) {
+            List<XWPFRun> runs = paragraph.getRuns();
+            for (XWPFRun run : runs) {
+                String text = run.getText(0);
+                if (text != null) {
+                    for (int i = 0; i < data.length; i++) {
+                        text = text.replace("{" + i + "}", data[i]);
+                    }
+                    run.setText(text, 0); // 更新文本内容
                 }
-                return o1.compareTo(o2);
             }
-        });
-        double px = p * (n - 1);
-        int i = (int) Math.floor(px);
-        double g = px - i;
-        if (g == 0) {
-            return dataList.get(i);
-        } else {
-            return (1 - g) * dataList.get(i) + g * dataList.get(i + 1);
         }
-    }
 
-    /**
-     * 求在百分位p的值是多少
-     *
-     * @param data
-     * @param p
-     * @return
-     */
-    public static double percentile(double[] data, double p) {
-        int n = data.length;
-        Arrays.sort(data);
-        double px = p * (n - 1);
-        int i = (int) Math.floor(px);
-        double g = px - i;
-        if (g == 0) {
-            return data[i];
+        // 插入图片到指定位置（这里以第一个表格为例）
+        List<XWPFTable> tables = document.getTables();
+        if (!tables.isEmpty()) {
+            XWPFTable table = tables.get(0); // 获取第一个表格
+            int rowIndex = 1; // 插入图片的行索引（从1开始）
+            int colIndex = 1; // 插入图片的列索引（从1开始）
+            insertImageToTable(table, rowIndex, colIndex, imagePath); // 插入图片到表格中指定位置的方法，需要自定义实现该方法，参考下面的代码示例
         } else {
-            return (1 - g) * data[i] + g * data[i + 1];
+            System.out.println("未找到表格");
         }
+
+        // 保存输出文件
+        FileOutputStream fos = new FileOutputStream(new File(outputPath));
+        document.write(fos); // 将修改后的文档写入输出文件流中，并关闭输出流和文档对象
+        fos.close();
+        document.close(); // 关闭文档对象，释放资源
     }
-
-
 }
